9bcce2998094c5f6cb525cdfcec9a8c4cf7b56bd
Giuseppe Ravidà <giusepperavida@MacBook-Pro-M1-di-Giuseppe.local>
2024-08-21 10:03:52 +0200

    Added Camera component

--- components/CameraScreen.tsx
+++ components/CameraScreen.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Camera, useCameraDevice, useCameraPermission } from "react-native-vision-camera";
+import { useIsFocused } from "@react-navigation/native";
+import { useAppState } from "@react-native-community/hooks";
 
 const CameraScreen = () => {
+    const device = useCameraDevice('back');
+    const { hasPermission, requestPermission } = useCameraPermission();
+
+    const isFocused = useIsFocused();
+    const currentAppState = useAppState();
+
+    useEffect(() => {
+        const getPermissions = async () => {
+            if(!hasPermission) await requestPermission();
+        };
+
+        getPermissions();
+    }, []);
+
+    if (device == null) return <Text>Camera Permissions Error</Text>
+    
     return (
-        <View style={styles.container}>
-            <Text>Camera Screen</Text>
-        </View>
+        <Camera
+            style={ StyleSheet.absoluteFill }
+            device={ device }
+            isActive={ isFocused && currentAppState === 'active' }
+        />
     )
 };
 
